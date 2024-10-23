079f893729df5683668d0d28bdef3685369bff3a
khokho <michealkosisochukwu300@gmail.com>
2024-10-23 12:46:53 +0100

    first stage

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,8 @@ function App() {
   return (
     <>
     <nav>
-      <h2 className="">Rick & Morty <span className="text-blue-500">Wiki</span></h2>
+      <h2 className="text-3xl text-red-700 p-2 text-bold">Rick & Morty <span className="text-blue-600 text-3xl">Wiki</span></h2>
+     <p className="text-1xl text-black-500 p-3">Life isn't a bed of roses,neither is a land full of milk and homey are keeping us in that situation</p>
     </nav>
     <main>
 
